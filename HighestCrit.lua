9da7896d13a2d4ddb3894785f326d77efa6bdd08
Jordan Bursik <jsbursik@gmail.com>
2024-12-27 14:15:55 -0600

    rewrote in Ace3, untested

--- HighestCrit.lua
+++ HighestCrit.lua
@@ -1,38 +1,36 @@
-local f = CreateFrame("Frame", nil, UIParent)
-local t = f:CreateFontString(nil, "OVERLAY", "GameTooltipText")
-f:SetSize(100, 50)
-f:SetPoint("TOP", 0, -100)
-t:SetPoint("CENTER", 0, 0)
-t:SetFont("fonts/2002.ttf", 20, "OUTLINE")
+HighestCrit = LibStub("AceAddon-3.0"):NewAddon("HighestCrit", "AceEvent-3.0")
 
-function f:OnEvent(event, ...)
-  self[event](self, event, ...)
+HighestCrit:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+
+local t = HighestCrit:CreateFontString(nil, "OVERLAY", "GameTooltipText")
+
+local defaults = {
+  profile = {
+    setting = true,
+    critAmt = 0,
+    displayFont = "fonts/2002.ttf",
+    displayFontSize = 20,
+    displayFontStyle = "OUTLINE",
+  },
+}
+
+local playerName, _ = UnitName("player")
+
+function HighestCrit:OnInitialize()
+  self.db = LibStub("AceDB-3.0"):New("HighestCritDB", defaults, true)
+  self.db:SetProfile(playerName)
+  t:SetPoint("CENTER", 0, 0)
+  t:SetFont(self.db.profile.displayFont, self.db.profile.displayFontSize, self.db.profile.displayFontStyle)
+  t:SetText("Highest Crit: ".. self.db.profile.critAmt)
 end
 
--- Handle any initialization here
-function f:ADDON_LOADED(event, addOnName)
-  if addOnName == "HighestCrit" then
-    HighestCritDB = HighestCritDB or {}
-    HighestCritDB.amt = HighestCritDB.amt or 0
-    t:SetText("Highest Crit: ".. tostring(HighestCritDB.amt))
-  end
-end
-
--- Any subevent that includes "DAMAGE" done by the player gets checked if it's a crit
-function f:COMBAT_LOG_EVENT_UNFILTERED(event, ...)
+function HighestCrit:COMBAT_LOG_EVENT_UNFILTERED(event, ...)
   local _, subevent, _, _, sourceName = CombatLogGetCurrentEventInfo()
-  local playerName, _ = UnitName("player")
-  if sourceName == playerName and string.find(subevent, "DAMAGE") ~= nil then
+  if sourceName == playerName and string.find(subevent, "DAMAGE") then
     local amount, _, _, _, _, _, critical = select(15, CombatLogGetCurrentEventInfo())
-    if critical == true and amount > HighestCritDB.amt then
-      HighestCritDB.amt = amount
-      t:SetText("Highest Crit: ".. HighestCritDB.amt)
+    if critical == true and amount > self.db.profile.critAmt then
+      self.db.profile.critAmt = amount
+      t:SetText("Highest Crit: ".. self.db.profile.critAmt)
     end
   end
 end
-
-f:RegisterEvent("ADDON_LOADED")
-f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-f:SetScript("OnEvent", f.OnEvent)
-
-
