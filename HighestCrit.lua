a77a0af825e8f02c3c043780e5a02d9c026328f5
Jordan Bursik <jsbursik@gmail.com>
2024-12-27 23:35:21 -0600

    Worked on movable frame/text

--- HighestCrit.lua
+++ HighestCrit.lua
@@ -1,8 +1,8 @@
 HighestCrit = LibStub("AceAddon-3.0"):NewAddon("HighestCrit", "AceEvent-3.0")
 
-HighestCrit:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-
-local t = HighestCrit:CreateFontString(nil, "OVERLAY", "GameTooltipText")
+CritFrame = CreateFrame("Frame", nil, UIParent)
+CritText = CritFrame:CreateFontString(nil, "OVERLAY", "GameTooltipText")
+CritText:SetPoint("CENTER", 0, 0)
 
 local defaults = {
   profile = {
@@ -11,17 +11,25 @@ local defaults = {
     displayFont = "fonts/2002.ttf",
     displayFontSize = 20,
     displayFontStyle = "OUTLINE",
+    XPos = 0,
+    YPos = 0,
   },
 }
 
 local playerName, _ = UnitName("player")
 
 function HighestCrit:OnInitialize()
+  LibStub("AceConfig-3.0"):RegisterOptionsTable("HighestCrit", HCOptions)
+  self.optionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("HighestCrit", "HighestCrit")
   self.db = LibStub("AceDB-3.0"):New("HighestCritDB", defaults, true)
   self.db:SetProfile(playerName)
-  t:SetPoint("CENTER", 0, 0)
-  t:SetFont(self.db.profile.displayFont, self.db.profile.displayFontSize, self.db.profile.displayFontStyle)
-  t:SetText("Highest Crit: ".. self.db.profile.critAmt)
+  self.XPos = self.db.profile.XPos
+  self.YPos = self.db.profile.YPos
+  CritFrame:SetPoint("CENTER", self.db.profile.XPos, self.db.profile.YPos)
+  self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+  CritText:SetFont(self.db.profile.displayFont, self.db.profile.displayFontSize, self.db.profile.displayFontStyle)
+  CritText:SetText("Highest Crit: ".. self.db.profile.critAmt)
+  CritFrame:SetSize(CritText:GetStringWidth(), CritText:GetStringHeight())
 end
 
 function HighestCrit:COMBAT_LOG_EVENT_UNFILTERED(event, ...)
@@ -30,7 +38,8 @@ function HighestCrit:COMBAT_LOG_EVENT_UNFILTERED(event, ...)
     local amount, _, _, _, _, _, critical = select(15, CombatLogGetCurrentEventInfo())
     if critical == true and amount > self.db.profile.critAmt then
       self.db.profile.critAmt = amount
-      t:SetText("Highest Crit: ".. self.db.profile.critAmt)
+      CritText:SetText("Highest Crit: ".. self.db.profile.critAmt)
+      CritFrame:SetSize(CritText:GetStringWidth(), CritText:GetStringHeight())
     end
   end
 end
--- HighestCritOptions.lua
+++ HighestCritOptions.lua
@@ -1,1 +1,51 @@
--- WIP
+local screenWidth, screenHeight = GetPhysicalScreenSize()
+
+HCOptions = {
+  name = "HighestCrit",
+  handler = HighestCrit,
+  type = "group",
+  args = {
+    posX = {
+      name = "X Position",
+      desc = "Determine the X position on the screen",
+      type = "range",
+      min = 0 - screenWidth / 2,
+      max = screenWidth / 2,
+      step = 1,
+      isPercent = false,
+      set = "SetXPos",
+      get = "GetXPos",
+    },
+    posY = {
+      name = "Y Position",
+      desc = "Determine the Y position on the screen",
+      type = "range",
+      min = 0 - screenHeight / 2,
+      max = screenHeight / 2,
+      step = 1,
+      isPercent = false,
+      set = "SetYPos",
+      get = "GetYPos",
+    }
+  }
+}
+
+function HighestCrit:SetXPos(info, value)
+  self.XPos = value
+  self.db.profile.XPos = self.XPos
+  CritFrame:SetPoint("CENTER", self.XPos, self.YPos)
+end
+
+function HighestCrit:GetXPos(info)
+  return self.XPos
+end
+
+function HighestCrit:SetYPos(info, value)
+  self.YPos = value
+  self.db.profile.YPos = self.YPos
+  CritFrame:SetPoint("CENTER", self.XPos, self.YPos)
+end
+
+function HighestCrit:GetYPos(info)
+  return self.YPos
+end
